26d34de3151a1f5dc157ba778677eafe0bac50b7
U-11 <mizu.w.w1.n23@gmail.com>
2020-10-18 19:10:57 +0900

    バリデーションの実装 閉じタグの削除

--- php-rt-iine/delete.php
+++ php-rt-iine/delete.php
@@ -2,18 +2,20 @@
 session_start();
 require('dbconnect.php');
 
-if(isset($_SESSION['id'])){
+if(isset($_SESSION['id']) && is_numeric($_SESSION['id'])){
+  if(isset($_REQUEST['id']) && is_numeric($_REQUEST['id'])){
 
-  $messages=$db->prepare('SELECT * FROM posts WHERE id=?');
-  $messages->execute(array($_REQUEST['id']));
-  $message=$messages->fetch();
-
-  if($message['member_id']==$_SESSION['id']){
-    $del=$db->prepare('UPDATE posts SET is_deleted=1 WHERE id=?');
-    $del->execute(array($_REQUEST['id']));
+    $messages=$db->prepare('SELECT * FROM posts WHERE id=?');
+    $messages->bindParam(1,$_REQUEST['id'],PDO::PARAM_INT);
+    $messages->execute();
+    $message=$messages->fetch();
+    
+    if($message['member_id']==$_SESSION['id']){
+      $del=$db->prepare('UPDATE posts SET is_deleted=1 WHERE id=?');
+      $del->execute(array($_REQUEST['id']));
+    }
   }
 }
 header('Location:index.php');
 exit();
 
-?>
